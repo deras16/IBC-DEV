a2bf168276567e6dc880924a38d2cbe69a6c1e62
Josías <josiasalvarengaromero@gmail.com>
2025-03-04 14:16:05 -0600

    adding toggle dark

---
+++ resources/js/Components/Core/ToggleDark.vue
@@ -0,0 +1,25 @@
+<script setup>
+import {isDark, toggleDarkMode} from "@/Composables/index.js";
+import IconButton from "@/Components/Core/Button/IconButton.vue";
+import {MoonIcon, SunIcon} from "@/Components/Core/Icons/BaseIcons.jsx";
+</script>
+
+<template>
+    <IconButton
+        @click="() => { toggleDarkMode() }"
+        v-slot="{ iconSizeClasses }"
+        class="p-2 hidden md:inline-flex"
+        srText="Toggle dark mode"
+    >
+        <MoonIcon
+            v-show="!isDark"
+            aria-hidden="true"
+            :class="iconSizeClasses"
+        />
+        <SunIcon
+            v-show="isDark"
+            aria-hidden="true"
+            :class="iconSizeClasses"
+        />
+    </IconButton>
+</template>
